096ec41ec27b24b8d7d1cf63b9a170b35737820d
Elise Soper <dracomurex@yahoo.com>
2022-08-11 15:13:04 -0400

    employee page boxes

--- vue/src/views/EmployeePage.vue
+++ vue/src/views/EmployeePage.vue
@@ -1,32 +1,53 @@
 <template>
-  <employee-review-pending v-bind:potholes="potholes"/>
+  <div id="container">
+    <section id="top-section">
+      <graph />
+      <pothole-card />
+    </section>
+    <section id="bottom-section">
+      <employee-review-pending v-bind:potholes="potholes" />
+      <repair-in-progress />
+    </section>
+  </div>
 </template>
 
 <script>
-import EmployeeReviewPending from '../components/EmployeeReviewPending.vue';
-import potHolesService from "../services/PotholesService.js";
-export default {
+import EmployeeReviewPending from "../components/EmployeeReviewPending.vue";
+import Graph from "../components/Graph.vue";
+import PotholeCard from "../components/PotholeCard.vue";
+import RepairInProgress from "../components/RepairInProgressTable.vue";
 
-    data(){
-        return{
-            potholes: []
-        }
-    },
-     created() {
+import potHolesService from "../services/PotholesService.js";
+
+export default {
+  data() {
+    return {
+      potholes: [],
+    };
+  },
+  created() {
     potHolesService.retrievePotholes().then((response) => {
       this.potholes = response.data;
     });
   },
 
-components: {
-    EmployeeReviewPending
-    
-}
-
-
-}
+  components: {
+    EmployeeReviewPending,
+    Graph,
+    PotholeCard,
+    RepairInProgress,
+  },
+};
 </script>
 
 <style>
+#container{
+    display:flex;
+    flex-direction: column;
+    justify-content: space-around;
+}
+#top-section, #bottom-section{
+background-color: yellowgreen;
+}
 
 </style>
