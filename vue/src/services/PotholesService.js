8dc5707b9243e5f19df58938029319bc24e40475
Dylan Christopher <dylan.j.christopher6@gmail.com>
2022-08-12 09:44:54 -0400

    update repair

--- vue/src/services/PotholesService.js
+++ vue/src/services/PotholesService.js
@@ -10,5 +10,10 @@ export default{
         console.log('blake')
         return axios.post('/pothole', pothole)
     },
+    updatePotholeRepair(pothole){
+        return axios.put('/pothole/{id}',pothole.potholeId)
+    }
+
+
     // Use tick-marks instead of quotes because we are trying to pass information inside the url
 }
